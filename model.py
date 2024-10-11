7a3f716c17a767433731b7563551e6f5185ea70c
Kavya Karthik <kkavya.jan30@gmail.com>
2024-10-10 22:49:24 -0700

    outlining components

--- model.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoModelForCTC, Wav2Vec2Processor
-
-model = AutoModelForCTC.from_pretrained("kavsss/wav2vec2-tune-timit-asr-for-phoneme")
-processor = Wav2Vec2Processor.from_pretrained("kavsss/wav2vec2-tune-timit-asr-for-phoneme")
-
-import torch
