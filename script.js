114024b3cc87a98265434ce2d17f719b4a2c8060
Kavya Karthik <kkavya.jan30@gmail.com>
2025-08-31 23:22:39 -0700

    cleaned up layout

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-// ordering to chronological
-document.getElementById("reorder").addEventListener("click", function() {
-    const container = document.getElementById("grid-container");
+let chronological = false;
+const container = document.getElementById("grid-container");
+const ogOrder = Array.from(container.querySelectorAll(".grid-item")); // stores custom order in array
+const reorderBt = document.getElementById("reorder");
+
+reorderBt.addEventListener("click", function() {
     const events = Array.from(container.querySelectorAll(".grid-item"));
 
-    events.sort((a, b) => {
-        const dateA = new Date(a.getAttribute("data-date"));
-        const dateB = new Date(b.getAttribute("data-date"));
-        return dateA - dateB;
-    });
+    // ordering chronologically
+    if (!chronological) {
+        events.sort((a, b) => {
+            const dateA = new Date(a.getAttribute("data-date"));
+            const dateB = new Date(b.getAttribute("data-date"));
+            return dateA - dateB;
+        });
+
+        reorderBt.textContent = "View Custom Order";
+        chronological = true;
+    } else { // ordering using custom order
+        events.splice(0, events.length, ...ogOrder);
+        reorderBt.textContent = "View Chronologically";
+        
+        chronological = false;
+    }
 
     events.forEach(event => container.appendChild(event));
 });
